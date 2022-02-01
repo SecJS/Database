4adb7d8f167197b038eee63c469ce2e058ecf7b1
João Lenon <lenonSec7@gmail.com>
2022-02-01 00:18:47 -0300

    styles: add todo on clients

--- src/Clients/Clients.ts
+++ src/Clients/Clients.ts
@@ -10,6 +10,14 @@
 import { KnexClient } from './KnexClient'
 import { MongooseClient } from './MongooseClient'
 
+// TODO Move clients code to Drivers again.
+//      Clients are very useful to not repeat code,
+//      but if we have compatibility problem with some database
+//      all the code is in clients, changing client code would break
+//      all the other drivers (high coupling).
+//      Is better to "repeat code" inside Drivers than to have a coupling problem later on.
+//      This refactor could come as an issue later, because for now Clients are useful
+//      because we only have PostgresDriver, SqliteDriver and MongoDriver.
 export const Clients = {
   knex: KnexClient,
   mongoose: MongooseClient,
